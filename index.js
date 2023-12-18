c9acf60fd179549f9293710acd8f7ce627faaf0c
Acolith <marcio.kiorio@gmail.com>
2023-12-18 20:48:30 -0300

    Níveis do Herói

---
+++ index.js
@@ -0,0 +1,14 @@
+var nome = "Acolith";
+var nivel = 8964;
+
+if(nivel <= 1000) {nivel = "Ferro"} 
+else if(nivel >= 1001 && nivel <= 2000) {nivel = "Bronze"}
+else if(nivel >= 2001 && nivel <= 6000) {nivel = "Prata"}
+else if(nivel >= 6001 && nivel <= 7000) {nivel = "Ouro"}
+else if(nivel >= 7001 && nivel <= 8000) {nivel = "Platina"}
+else if(nivel >= 8001 && nivel <= 9000) {nivel = "Ascendente"}
+else if(nivel >= 9001 && nivel <= 10000) {nivel = "Imortal"}
+  else {nivel = "Radiante" 
+  }
+
+console.log('O Herói de nome ' + (nome) + ' está no nível ' + (nivel) + '.');
