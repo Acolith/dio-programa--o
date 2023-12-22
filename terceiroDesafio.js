eb85682f5441c535789eeb8600caac7594ce6d54
Acolith <marcio.kiorio@gmail.com>
2023-12-22 15:23:27 -0300

    Melhorias no Desafio 3

--- terceiroDesafio.js
+++ terceiroDesafio.js
@@ -1,14 +1,21 @@
-class dadosDoPersonagem{
-    constructor(nome, idade, tipo){
+class criarPersonagem{
+    contructor(nome, idade, tipo, ataque) {
         this.nome = nome
         this.idade = idade
         this.tipo = tipo
+        this.ataque = ataque
     }
+
+    atacar(){
+        return action
+    }
+        
     toString(){
         console.log(`Você criou um personagem chamado ${this.nome} de ${this.idade} anos e escolheu a classe ${this.tipo}.`)
+        console.log(action)
     }
 }
 
-let escolhaPersonagem = new dadosDoPersonagem("Acolith", 39, "mago")
-
+let action = `O ${this.tipo} atacou usando ${this.ataque}`
+let escolhaPersonagem = new criarPersonagem()
 escolhaPersonagem.toString()
